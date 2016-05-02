54ea606ce706324e63b54c9e53a8d19043ac6e03
Matthias Langhard <matthias.langhard@ecologic.ch>
2016-05-02 13:26:46 +0200

    lab-server

---
+++ lab-server/src/main/java/ch/fhnw/kvanc/LabServerApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.kvanc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabServerApplication.class, args);
+	}
+}
